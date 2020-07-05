4d6718037fcb1f5d0d2d0e6770c0ef2b566d220a
duy ha <duyhph@gmail.com>
2020-07-05 16:28:02 +0300

    reverse

--- shops.go
+++ shops.go
@@ -13,7 +13,6 @@ type Shop struct {
 	Phone   string          `bson:"phone"`
 	Created time.Time       `bson:"created"`
 	Config  ShopConfigs     `bson:"config"`
-	Level   ShopLevel       `bson:"level"`
 	Status  int             `bson:"status"`
 	Theme   string          `bson:"theme"`
 	Modules map[string]bool `bson:"modules"`
@@ -25,15 +24,6 @@ type ShopConfigs struct {
 	Type        bool     `bons:"type"`
 	Langs       []string `bson:"langs"`
 	DefaultLang string   `bson:"defaultlang"`
-}
-type ShopLevel struct {
-	Package  string `bson:"package"`
-	MaxUser  int    `bson:"maxuser"`
-	MaxImage int    `bson:"maximage"`
-	MaxAlbum int    `bson:"maxalbum"`
-	MaxCat   int    `bson:"maxcat"`
-	MaxProd  int    `bson:"maxprod"`
-	MaxNews  int    `bson:"maxnews"`
 }
 type ShopLimit struct {
 	ID     bson.ObjectId `bson:"_id,omitempty"`
