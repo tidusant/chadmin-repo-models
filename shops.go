75ba47279a7ff342b5d3f91332b9ea8afd752b1c
duy ha <duyhph@gmail.com>
2020-07-05 16:27:03 +0300

    create

--- shops.go
+++ shops.go
@@ -13,6 +13,7 @@ type Shop struct {
 	Phone   string          `bson:"phone"`
 	Created time.Time       `bson:"created"`
 	Config  ShopConfigs     `bson:"config"`
+	Level   ShopLevel       `bson:"level"`
 	Status  int             `bson:"status"`
 	Theme   string          `bson:"theme"`
 	Modules map[string]bool `bson:"modules"`
@@ -24,6 +25,15 @@ type ShopConfigs struct {
 	Type        bool     `bons:"type"`
 	Langs       []string `bson:"langs"`
 	DefaultLang string   `bson:"defaultlang"`
+}
+type ShopLevel struct {
+	Package  string `bson:"package"`
+	MaxUser  int    `bson:"maxuser"`
+	MaxImage int    `bson:"maximage"`
+	MaxAlbum int    `bson:"maxalbum"`
+	MaxCat   int    `bson:"maxcat"`
+	MaxProd  int    `bson:"maxprod"`
+	MaxNews  int    `bson:"maxnews"`
 }
 type ShopLimit struct {
 	ID     bson.ObjectId `bson:"_id,omitempty"`
